289ad31572d51b15a5d63932d722568f1716b72f
Makar90 <Makarlancer@gmail.com>
2023-03-10 01:46:29 +0200

    123

--- Final_project/front/src/components/playField/PlayField.jsx
+++ Final_project/front/src/components/playField/PlayField.jsx
@@ -3,23 +3,47 @@ import './index.css';
 import InnerArea from '../playField/innerArea/InnerArea';
 import CardCommon from '../playSteps/_cardCommon/CardCommon';
 import {CardsData} from '../../data/CardsData';
-console.log(CardsData[0]);
+
+
+import imaga from '../../img/cards/front-media.jpg';
+
+console.log(CardsData[0].faceBackground);
 
 export default function PlayField(){
     let cards=[];
-    for(let i=1; i<=39; i++){
+    for(let i=12; i<=40; i++){
         cards.push(<CardCommon  key={i+1} cardName={i} cardImage='#1' cardPrice='2500' cardOwner="Igor"/>)
     }
 
+    
+    const cardAreaStyle = {
+        //backgroundImage: `url(${imaga})`, //`url(${CardsData[0].faceBackground})`,
+        backgroundImage: `url('./img/cards/front-suplying.jpg')`,
+        //backgroundImage:{require ('../../img/cards/front-media.jpg')},
+        backgroundSize: 'cover',
+    };
+
     return(
         <div className='play-field'>
-            
-            <CardCommon key={CardsData[0].id} 
+            {CardsData.map((item, index) => <CardCommon 
+                        key={item.id} 
+                        styles={{backgroundImage: `url(${item.faceBackground})`}}
+                        /* styles={cardAreaStyle}   */
+                                                                   
+                        cardType={item.type} 
+                        cardName={item.name} 
+                        cardImage='#1' 
+                        cardPrice={item.price}  
+                        cardOwner="Igor"/>
+                        )
+                        }
+
+            {/* <CardCommon key={CardsData[0].id} 
                         cardType={CardsData[0].type} 
                         cardName={CardsData[0].name} 
                         cardImage='#1' 
                         cardPrice={CardsData[0].price}  
-                        cardOwner="Igor"/>
+                        cardOwner="Igor"/> */}
             {cards}
             <InnerArea/>
         </div>
--- Final_project/front/src/data/CardsData.jsx
+++ Final_project/front/src/data/CardsData.jsx
@@ -2,19 +2,21 @@ import '../img/cards/front-media.jpg'
 
 export const CardsData=[
     {
-        id:'1',
-        faceBackground:'../img/cards/front-media.jpg',
+        id:1,
+        faceBackground:'/img/cards/front-cornersCard.jpg',
         type:'Старт',
         name:'',        
         img:'../img/cards/media-toronto.jpg',
         price:'',        
         
-        backBackground:'../img/cards/back.jpg',
+        backBackground:'/img/cards/back.jpg',
         priceSold:'',
-        priceIncoming:['']        
+        priceIncoming:[
+            ''
+        ]        
     },
     {
-        id:'2',
+        id:2,
         faceBackground:'../img/cards/front-media.jpg',
         type:'ЗМІ',
         name:'Телебачення торонто',        
@@ -31,7 +33,7 @@ export const CardsData=[
         ]        
     },
     {
-        id:'3',
+        id:3,
         faceBackground:'../img/cards/front-zsu.jpg',
         type:'ЗСУ',
         name:'АЗОВ',        
@@ -45,6 +47,131 @@ export const CardsData=[
             1000,
             2000,
             3500
+        ]        
+    },
+    {
+        id:4,
+        faceBackground:'../img/cards/front-trashManufactury.jpg',
+        type:'Виробництво пакетів',
+        name:'',        
+        img:'../img/cards/icon-trashManufactury.png',
+        price:1000,        
+        
+        backBackground:'../img/cards/back.jpg',
+        priceSold:600,
+        priceIncoming:[
+            200,
+            600,
+            1200
+        ]        
+    },
+    {
+        id:5,
+        faceBackground:'../img/cards/front-municipalTreasures.jpg',
+        type:'Міська скарбниця',
+        name:'',        
+        img:'../img/cards/icon-municipalTreasures.png',
+        price:'',        
+        
+        backBackground:'../img/cards/back.jpg',
+        priceSold:'',
+        priceIncoming:[
+            ''
+        ]        
+    },
+    {
+        id:6,
+        faceBackground:'../img/cards/front-humanitaryHelp.jpg',
+        type:'Гуманітарна допомога',
+        name:'Західна Україна',        
+        img:'../img/cards/icon-humanitaryHelp.png',
+        price:1000,        
+        
+        backBackground:'../img/cards/back.jpg',
+        priceSold:600,
+        priceIncoming:[
+            200,
+            600,
+            1200,
+            2000
+        ]        
+    },
+    {
+        id:7,
+        faceBackground:'../img/cards/front-volonterOrganization.jpg',
+        type:'Волонтерська організація',
+        name:'Фонд С. Притули',        
+        img:'../img/cards/icon-volonterOrganization-Prytula.png',
+        price:1500,        
+        
+        backBackground:'../img/cards/back.jpg',
+        priceSold:800,
+        priceIncoming:[
+            300,
+            900,
+            1600,
+            2700
+        ]        
+    },
+    {
+        id:8,
+        faceBackground:'../img/cards/front-chance.jpg',
+        type:'Шанс',
+        name:'',        
+        img:'../img/cards/icon-chance.png',
+        price:'',        
+        
+        backBackground:'../img/cards/back.jpg',
+        priceSold:'',
+        priceIncoming:[
+            ''
+        ]        
+    },
+    {
+        id:9,
+        faceBackground:'../img/cards/front-strategyObject.jpg',
+        type:'Стратегічний об\'єкт',
+        name:'Запорізька АЕС-1',        
+        img:'../img/cards/icon-strategyObject-ZAES-1.png',
+        price:3000,        
+        
+        backBackground:'../img/cards/back.jpg',
+        priceSold:2000,
+        priceIncoming:[
+            800,
+            2000,
+            3000,
+            4500
+        ]        
+    },
+    {
+        id:10,
+        faceBackground:'../img/cards/front-suplying.jpg',
+        type:'Постачання',
+        name:'Лендліз США',        
+        img:'../img/cards/icon-suplying-USA.png',
+        price:4000,        
+        
+        backBackground:'../img/cards/back.jpg',
+        priceSold:2000,
+        priceIncoming:[
+            1200,
+            2800,
+            5500
+        ]        
+    },
+    {
+        id:11,
+        faceBackground:'../img/cards/front-cornersCard.jpg',
+        type:'Бомбосховище',
+        name:'',        
+        img:'../img/cards/icon-cornersCard-bombShelter.png',
+        price:'',        
+        
+        backBackground:'../img/cards/back.jpg',
+        priceSold:'',
+        priceIncoming:[
+            ''
         ]        
     },
 ];
