326fa532c14fe53c014fccb133dbd7de2e4a88ee
Makar90 <Makarlancer@gmail.com>
2023-03-09 22:31:23 +0200

    rrr

--- Final_project/front/src/common/main/Main.jsx
+++ Final_project/front/src/common/main/Main.jsx
@@ -1,6 +1,6 @@
 import './index.css'
 import PlayField from '../../components/playField/PlayField';
-import CardCommon from '../../components/playSteps/_cardCommon/CardCommon';
+//import CardCommon from '../../components/playSteps/_cardCommon/CardCommon';
 
 export default function Main() {
     return(
